d15eb6b6725ff543b210b4823fd5a64c32e5b206
alderie <victornovich@yahoo.com>
2019-12-26 23:27:40 -0500

    Simplify the Database manager, add some basic functionality to inventory manager

--- src/com/twostudentsllc/gladiator/global/DatabaseManager.java
+++ src/com/twostudentsllc/gladiator/global/DatabaseManager.java
@@ -1,11 +1,11 @@
 package com.twostudentsllc.gladiator.global;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_zh_TW;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -17,6 +17,90 @@ import java.util.zip.GZIPOutputStream;
  *
  */
 public class DatabaseManager {
+
+	/**
+	 * Saves a serializable object to a filepath
+	 * @param object Any object that implements the serializable interface
+	 * @param filePath the path where the object will be written to
+	 */
+	public static void saveSerializableObject(Serializable object, String filePath) {
+
+		File file = new File(filePath);
+
+		System.out.println("Actual file path: '" + file.getPath() + "'");
+
+		//If the file doesnt exist
+		if(!file.exists()) {
+			try {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		ObjectOutputStream output = null;
+		try {
+			output = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(file)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//If it was unable to load the file
+		if(output == null)
+			return;
+
+		try {
+			output.writeObject(object);
+			output.flush();
+			output.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Loads a serialized object from a file
+	 * @param filePath where to load the object from
+	 * @return Object the serialized object
+	 */
+	public static Object loadObjectFromFile(String filePath) {
+
+		File file = new File(filePath);
+
+		System.out.println("Actual file path: '" + file.getPath() + "'");
+		//If the file doesnt exist
+		if(!file.exists()) {
+			try {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+				return new HashMap<String, String>();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Object readObject = null;
+		//Gets the files information
+		try {
+			FileInputStream stream = new FileInputStream(file);
+			GZIPInputStream GZIP = new GZIPInputStream(stream);
+			ObjectInputStream input = new ObjectInputStream(GZIP);
+			readObject = input.readObject();
+			input.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//If it didnt load
+		if(readObject == null)
+			return null;
+
+		return readObject;
+	}
 	
 	public static void saveLocations(HashMap<String, String> locations, String minigameName, String mapName)
 	{
@@ -24,7 +108,10 @@ public class DatabaseManager {
 		String fileName = getLocationFileNameString(mapName);
 		
 		System.out.println("Saving location data for minigame '" + minigameName  + "' and map '" + mapName + "' at file: '" + fileDir + fileName + "'!");
-		
+
+		String filePath = fileDir + fileName;
+		saveSerializableObject(locations, filePath);
+/*
 //		File dir = new File(fileDir);
 //		//If the directory doesnt exist, create it.
 //		if(!dir.exists())
@@ -66,7 +153,7 @@ public class DatabaseManager {
 		catch(Exception e)
 		{
 			e.printStackTrace();
-		}
+		}*/
 		
 	}
 	
@@ -76,47 +163,10 @@ public class DatabaseManager {
 		String fileName = getDataFileNameString(fileN);
 		
 		System.out.println("Saving map data for minigame '" + minigameName  + "' at file: '" + fileDir + fileName + "'!");
-		
-		File file = new File(fileDir + fileName);
-		
-		System.out.println("Actual file path: '" + file.getPath() + "'");
-		
-//		File dir = new File(fileDir);
-//		//If the directory doesnt exist, create it.
-//		if(!dir.exists())
-//		{
-//			dir.mkdirs();
-//		}
-		
-		if(!file.exists()) {
-			try {
-				file.getParentFile().mkdirs();
-			    file.createNewFile();
-			} catch(IOException e) {
-			    e.printStackTrace();
-			}
-		}
-		
-		ObjectOutputStream output = null;
-		try {
-			output = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(file)));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//If it was unable to load the file
-		if(output == null)
-			return;
-		
-		try {
-			output.writeObject(serializedMaps);
-			output.flush();
-			output.close();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+
+		String filePath = fileDir + fileName;
+		saveSerializableObject(serializedMaps, filePath);
+
 	}
 	
 	public static HashMap<String, String> loadLocations(String minigameName, String mapName)
@@ -125,14 +175,22 @@ public class DatabaseManager {
 		String fileName = getLocationFileNameString(mapName);
 		
 		System.out.println("Loading location data for minigame '" + minigameName  + "' and map '" + mapName + "' at file: '" + fileDir + fileName + "'!");
+
+		String filePath = fileDir + fileName;
+
+		//INFO: I don't think we should catch classcastexceptions since they are our fault
+		return (HashMap<String, String>) loadObjectFromFile(filePath);
+
 //		File dir = new File(fileDir);
 //		//If the directory doesnt exist, create it.
 //		if(!dir.exists())
 //		{
 //			dir.mkdirs();
 //		}
+
+
 		
-		File file = new File(fileDir + fileName);
+		/*File file = new File(fileDir + fileName);
 		
 		System.out.println("Actual file path: '" + file.getPath() + "'");
 		//If the file doesnt exist
@@ -173,7 +231,7 @@ public class DatabaseManager {
 //			return null;
 //		}
 		
-		return (HashMap<String, String>) readObject;
+		return (HashMap<String, String>) readObject;*/
 		
 	}
 	
@@ -187,58 +245,44 @@ public class DatabaseManager {
 		String fileName = getDataFileNameString(fileN);
 		
 		System.out.println("Loading map data for minigame '" + minigameName  + "' at file: '" + fileDir + fileName + "'!");
+
+		String filePath = fileDir + fileName;
+
+		return (HashMap<String, String>) loadObjectFromFile(filePath);
 		
-//		File dir = new File(fileDir);
-//		//If the directory doesnt exist, create it.
-//		if(!dir.exists())
-//		{
-//			dir.mkdirs();
-//		}
-		
-		File file = new File(fileDir + fileName);
-		
-		System.out.println("Actual file path: '" + file.getPath() + "'");
-		//If the file doesnt exist
-		if(!file.exists()) {
-			System.out.println("Maps HashMap does not exist! Creating an empty one.");
-			try {
-				file.getParentFile().mkdirs();
-			    file.createNewFile();
-			    return new HashMap<String, String>();
-			} catch(IOException e) {
-			    e.printStackTrace();
-			}
-		}
-		
-		Object readObject = null;
-		//Gets the files information
-		try {
-			FileInputStream stream = new FileInputStream(file);
-			GZIPInputStream GZIP = new GZIPInputStream(stream);
-			ObjectInputStream input = new ObjectInputStream(GZIP);
-			readObject = input.readObject();
-			input.close();
-		} catch (IOException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//If it didnt load
-		if(readObject == null)
-			return null;
-		//If the object read is not a hashmap
-//		if(!(readObject instanceof GameMap))
-//		{
-//			try {
-//				throw new IOException("Object in " + fileName + " is not a HashMap.");
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			return null;
-//		}
-		
-		return (HashMap<String, String>) readObject;
-		
+	}
+
+	/**
+	 * Save custom inventories for a minigame
+	 * @param inventoryList HashMap of custom inventories
+	 * @param minigameName Name of the minigame
+	 */
+	public static void saveInventories(HashMap<String, PlayerInventory> inventoryList, String minigameName) {
+
+		String fileDir = getDatabaseDirectoryString(minigameName);
+		String fileName = getDataFileNameString("inventories");
+
+		System.out.println("Saving inventory data for minigame '" + minigameName  + "' at file: '" + fileDir + fileName + "'!");
+
+		String filePath = fileDir + fileName;
+
+		saveSerializableObject(inventoryList, filePath);
+	}
+
+	/**
+	 * Loads custom inventories for a minigame
+	 * @param minigameName Name of the minigame
+	 * @return HashMap of inventories mapped by custom names to the player inventories
+	 */
+	public static HashMap<String, PlayerInventory> loadInventories(String minigameName) {
+		String fileDir = getDatabaseDirectoryString(minigameName);
+		String fileName = getDataFileNameString("inventories");
+
+		System.out.println("Loading inventory data for minigame '" + minigameName  + "' at file: '" + fileDir + fileName + "'!");
+
+		String filePath = fileDir + fileName;
+
+		return (HashMap<String, PlayerInventory>)loadObjectFromFile(filePath);
 	}
 	
 	
--- src/com/twostudentsllc/gladiator/managers/InventoryManager.java
+++ src/com/twostudentsllc/gladiator/managers/InventoryManager.java
@@ -3,37 +3,78 @@ package com.twostudentsllc.gladiator.managers;
 import java.util.HashMap;
 import java.util.UUID;
 
+import com.twostudentsllc.gladiator.global.DatabaseManager;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 import com.twostudentsllc.gladiator.Main;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
 
 public class InventoryManager {
 	
-	HashMap<UUID, Inventory> inventoriesSaved;
+	private HashMap<String, PlayerInventory> savedInventories;
+	private String minigameName;
 	
 	private Main plugin;
-	public InventoryManager(Main plugin)
+	public InventoryManager(Main plugin, String minigameName)
 	{
+		HashMap<String, PlayerInventory> externalFile = DatabaseManager.loadInventories(minigameName);
+
+		if(externalFile!=null)
+			savedInventories = externalFile;
+
 		this.plugin = plugin;
-		inventoriesSaved = new HashMap<UUID, Inventory>();
+		savedInventories = new HashMap<String, PlayerInventory>();
+		this.minigameName = minigameName;
 	}
-	
-	public void saveInventory(Player p)
+
+	/**
+	 * Saves a player's inventory to the hashMap
+	 * @param p player Object
+	 * @param nameToSave String name to save the inventory under
+	 */
+	public void saveInventory(@NotNull Player p, @NotNull String nameToSave)
 	{
-		//Get player inventory, create deep copy, set it the inventory variable
-		//get player UUID
-		//p.getUniqueId();
-		
-		//inventoriesSaved.put(<PlayerUUID>, <PlayerInv>);
+		PlayerInventory inventory = p.getInventory();
+		savedInventories.put(nameToSave, inventory);
+		DatabaseManager.saveInventories(savedInventories, minigameName);
 	}
-	
-	public Inventory getInventory(Player p, boolean forgetInventory)
+
+	/**
+	 * Gets a player's inventory
+	 * @param nameToSave Name to retrieve the player's inventory under
+	 * @param forgetInventory whether to forget the inventory once it is pulled
+	 * @return PlayerInventory object
+	 */
+	public PlayerInventory getInventory(@NotNull String nameToSave, boolean forgetInventory)
 	{
+		//Forget the inventory option
+		if(forgetInventory)
+			return savedInventories.remove(nameToSave);
+
+		return savedInventories.get(nameToSave);
+
 		//if the inventory should be removed from the database (if (forgetInventory)), then remove and return it using UUID.
 		//inventoriesSaved.get(players UUID);
-		return null; //Remove this line and replace it so that it returns the 
+		//Remove this line and replace it so that it returns the
 					 //players saved inventory from the inventoriesSaved
+	}
+
+	/**
+	 * Sets a player's inventory to one of the stored inventories
+	 * @param toSet Player object whose inventory should be set
+	 * @param targetInventoryName Name of the inventory the player should be set to
+	 */
+	public void setPlayerInventory(Player toSet, String targetInventoryName) {
+
+		PlayerInventory playerInv = toSet.getInventory();
+		PlayerInventory targetInv = savedInventories.get(targetInventoryName);
+
+		if(targetInv==null)
+			return;
+
+		playerInv.setContents(targetInv.getContents());
 	}
 	
 	
